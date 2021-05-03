dc3609ace86e052a8d5f3b7882a3190cc521bdf7
masato.tanaka <masato.t2207@gmail.com>
2021-05-03 20:48:17 +0900

    Add: ログイン周りのパスを追加

--- src/config/routes.rb
+++ src/config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
 	root 'tops#index'
-	devise_for :users
+	devise_for :users, controllers: {
+		registrations: 'users/registrations',
+		passwords: 'users/passwords'
+	}
 end
