dcc009633d9dce0c5a5ffe867e29eeebdaee26ef
masato.tanaka <masato.t2207@gmail.com>
2021-05-04 14:26:14 +0900

    Add: Userモデルバリデーションの追加

--- src/app/models/user.rb
+++ src/app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  # バリデーション 
+  before_save { self.email = email.downcase }
+  validates :email,     {presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }}
+  validates :password,  {length: { in: 8..32 }, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]/}}
+  with_options presence: true do
+    validates :last_name
+    validates :first_name
+  end
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 end
